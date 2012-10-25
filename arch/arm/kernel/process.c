e160c0f0c854ab921cb5c207c2f4fa295b9a5c9e
forumber <kidobeko@gmail.com>
2012-10-25 20:59:20 +0300

    ARM: idle: call idle notifiers before stopping nohz tick

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -146,8 +146,8 @@ void cpu_idle(void)
 
 	/* endless idle loop with no priority at all */
 	while (1) {
-		tick_nohz_stop_sched_tick(1);
 		leds_event(led_idle_start);
+		tick_nohz_stop_sched_tick(1);
 		while (!need_resched()) {
 #ifdef CONFIG_HOTPLUG_CPU
 			if (cpu_is_offline(smp_processor_id()))
