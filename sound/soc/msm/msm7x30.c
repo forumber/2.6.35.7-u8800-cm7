917067c863edf686115a9e266f08847ba5f5c3fd
forumber <kidobeko@gmail.com>
2013-02-26 20:10:07 +0200

    force phone mic with headphones & fix speakerphone echo by ksatta

--- sound/soc/msm/msm7x30.c
+++ sound/soc/msm/msm7x30.c
@@ -56,11 +56,13 @@ char snddev_name[AUDIO_DEV_CTL_MAX_DEV][44];
 static int device_index; /* Count of Device controls */
 static int simple_control; /* Count of simple controls*/
 
-static int headset_mic_switch = 1; // use phone's mic by default
-static int speakerphone_echo_fix = 1; // fix enabled by default
+static int headset_mic_switch = 0; // use phone's mic by default
+static int sound_logging = 0;
+// static int speakerphone_echo_fix = 1; // fix enabled by default
 
 module_param(headset_mic_switch,int,00644);
-module_param(speakerphone_echo_fix,int,00644);
+module_param(sound_logging,int,00644);
+// module_param(speakerphone_echo_fix,int,00644); probably not needed for official 2.3
 
 static int msm_scontrol_count_info(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_info *uinfo)
@@ -206,7 +208,6 @@ static int msm_volume_put(struct snd_kcontrol *kcontrol,
 	/* Only Decoder volume control supported */
 	session_mask = (0x1 << (session_id) << (8 * ((int)AUDDEV_CLNT_DEC-1)));
 	msm_vol_ctl.volume = volume;
-
 	broadcast_event(AUDDEV_EVT_STREAM_VOL_CHG, DEVICE_IGNORE,
 							session_mask);
 
@@ -252,9 +253,6 @@ static int msm_voice_put(struct snd_kcontrol *kcontrol,
 		return -EFAULT;
 	}
 
-
-
-
 	msm_set_voc_route(rx_dev_info, AUDIO_ROUTE_STREAM_VOICE_RX,
 				rx_dev_id);
 
@@ -266,13 +264,16 @@ static int msm_voice_put(struct snd_kcontrol *kcontrol,
 	/* Tx Device Routing */
 	tx_dev_id = ucontrol->value.integer.value[1];
   
-	// replace headset mic with speaker_mono_tx (speakerphone mic)
-//	if (headset_mic_switch && tx_dev_id == 3)
-//	  tx_dev_id = 12;
+	if (sound_logging)
+	  pr_info("sound_logging: msm_voice_put() tx_dev_id:%Lu rx_dev_id:%Lu \n", (long long unsigned int) tx_dev_id, (long long unsigned int) rx_dev_id);
 	
-	// replace speaker_dual_mic_broadside_tx dev_id:15 with speaker_mono_tx (12)
-//	if (speakerphone_echo_fix && tx_dev_id == 15)
-//	  tx_dev_id = 12;
+	// replace headset mic with speakerphone mic
+	if (headset_mic_switch && tx_dev_id == 3)
+	  tx_dev_id = 12;
+	
+	// replace speaker_dual_mic_broadside_tx dev_id:12 with speaker_mono_tx (10)
+	/*if (speakerphone_echo_fix && tx_dev_id == 12)
+	  tx_dev_id = 10;*/ // probably not needed in official 2.3
 	
 	tx_dev_info = audio_dev_ctrl_find_dev(tx_dev_id);
 
@@ -287,9 +288,6 @@ static int msm_voice_put(struct snd_kcontrol *kcontrol,
 		MM_ERR("Second Dev is supposed to be Tx\n");
 		return -EFAULT;
 	}
-
-
-
 
 	msm_set_voc_route(tx_dev_info, AUDIO_ROUTE_STREAM_VOICE_TX,
 				tx_dev_id);
@@ -338,13 +336,16 @@ static int msm_device_put(struct snd_kcontrol *kcontrol,
 	set = ucontrol->value.integer.value[0];
 	route_cfg.dev_id = ucontrol->id.numid - device_index;
 	
-	// replace headset mic (3) with speaker_mono_tx (10)
-//	if (headset_mic_switch && route_cfg.dev_id == 3)
-//	  route_cfg.dev_id=10;
+	if (sound_logging)
+	  pr_info("sound_logging: msm_device_put() route_cfg.dev_id:%d enable:%d \n", route_cfg.dev_id, set);
+	
+	// replace headset mic (3) with speaker phone mic (12)
+	if (headset_mic_switch && route_cfg.dev_id == 3)
+	  route_cfg.dev_id=12;
 	
 	// replace speaker_dual_mic_broadside_tx dev_id:12 with speaker_mono_tx (10)
-//	if (speakerphone_echo_fix && route_cfg.dev_id==12)
-//	  route_cfg.dev_id=10;
+	/*if (speakerphone_echo_fix && route_cfg.dev_id==12)
+	  route_cfg.dev_id=10; // probably not needed by official 2.3*/
 		
 	dev_info = audio_dev_ctrl_find_dev(route_cfg.dev_id);
 	if (IS_ERR(dev_info)) {
@@ -353,8 +354,6 @@ static int msm_device_put(struct snd_kcontrol *kcontrol,
 		return rc;
 	}
 	
-	MM_INFO("device %s set %d\n", dev_info->name, set);
-
 	if (set) {
 		if (!dev_info->opened) {
 			set_freq = dev_info->sample_rate;
@@ -469,8 +468,6 @@ static int msm_route_put(struct snd_kcontrol *kcontrol,
 	else
 		route_cfg.stream_type =	AUDIO_ROUTE_STREAM_REC;
 
-
-
 	dev_info = audio_dev_ctrl_find_dev(route_cfg.dev_id);
 
 	if (IS_ERR(dev_info)) {
@@ -524,11 +521,7 @@ static int msm_route_put(struct snd_kcontrol *kcontrol,
 						session_id,
 						SNDDEV_CAP_TX,
 						AUDDEV_CLNT_ENC);
-
-
-
 				if ((rc <= 0) || (enc_freq != requested_freq)) {
-
 					rc = msm_snddev_withdraw_freq
 						(session_id,
 						SNDDEV_CAP_TX, AUDDEV_CLNT_ENC);
@@ -589,8 +582,6 @@ static int msm_device_volume_put(struct snd_kcontrol *kcontrol,
 	int dev_id = ucontrol->value.integer.value[0];
 	int volume = ucontrol->value.integer.value[1];
 
-
-
 	dev_info = audio_dev_ctrl_find_dev(dev_id);
 
 	if (IS_ERR(dev_info)) {
@@ -599,9 +590,6 @@ static int msm_device_volume_put(struct snd_kcontrol *kcontrol,
 				PTR_ERR(dev_info));
 		return rc;
 	}
-
-
-
 
 	if (dev_info->dev_ops.set_device_volume)
 		rc = dev_info->dev_ops.set_device_volume(dev_info, volume);
